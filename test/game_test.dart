4f2469409377cfc04a7daf6d4b83f8470dcbe2a4
Ryohei UEDA <uedaryohei0421@gmail.com>
2022-08-18 06:52:14 +0900

    Extract game logic to another class (#9)

--- test/game_test.dart
+++ test/game_test.dart
@@ -1,10 +1,10 @@
-import 'package:fifteen_puzzle/game.dart';
+import 'package:fifteen_puzzle/play.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('shows initial score', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: Game()));
+    await tester.pumpWidget(const MaterialApp(home: Play()));
 
     expect(find.text('Moves: 0'), findsOneWidget);
   });
