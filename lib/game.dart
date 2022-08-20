402536077ebc96e94f850444eec4675865c9d851
Ryohei UEDA <uedaryohei0421@gmail.com>
2022-08-20 16:22:51 +0900

    Count and display playtime (#12)

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:collection/collection.dart';
+import 'playtime_counter.dart';
 import 'tile.dart';
 
 class Game {
@@ -8,15 +9,42 @@ class Game {
   final int gridSize;
   final int? seed;
   final List<Tile> tiles;
+  final PlaytimeCounter playtimeCounter;
+
   int moveCount = 0;
 
   Game({this.gridSize = 4, this.seed = 5555})
-      : tiles = generateShuffledTiles(gridSize, seed);
+      : tiles = generateShuffledTiles(gridSize, seed),
+        playtimeCounter = PlaytimeCounter();
 
   static List<Tile> generateShuffledTiles(int gridSize, int? seed) {
     final tiles = List.generate(gridSize * gridSize, (i) => Tile(number: i));
     tiles.shuffle(Random(seed));
     return tiles;
+  }
+
+  bool firstMove() {
+    return moveCount == 0;
+  }
+
+  void startCountingPlaytime() {
+    playtimeCounter.start();
+  }
+
+  void stopCountingPlaytime() {
+    playtimeCounter.stop();
+  }
+
+  /// Returns playtime formatted in `mm:ss`.
+  String playtime() {
+    return playtimeCounter
+        .duration()
+        .toString()
+        .split(".")
+        .first
+        .split(":")
+        .sublist(1)
+        .join(":");
   }
 
   void incrementMoveCount() {
